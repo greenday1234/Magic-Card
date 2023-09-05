3e3ed7274c740c0dff65cc91a3accbd29f374029
Lee ChanHui <ing3616062@naver.com>
2023-09-06 00:18:35 +0900

    Magic Card

---
+++ Magic Card/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Magic Card
+//
+//  Created by 이찬희 on 2023/09/06.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
